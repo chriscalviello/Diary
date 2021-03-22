ee2a70a266137c14e841ca8134a43463da56dee4
Chris Calviello <chris.calviello@gmail.com>
2021-03-22 14:55:47 +0100

    add 'created_at' property

--- Backend/src/models/comment/index.ts
+++ Backend/src/models/comment/index.ts
@@ -2,10 +2,12 @@ export class Comment {
   id: string;
   title: string;
   body: string;
+  created_at: Date;
 
   constructor(id: string, title: string, body: string) {
     this.id = id;
     this.title = title;
     this.body = body;
+    this.created_at = new Date();
   }
 }
