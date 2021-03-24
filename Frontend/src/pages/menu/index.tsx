e958ad22bd2370d17c52c0391a1630578d7af689
Chris Calviello <chris.calviello@gmail.com>
2021-03-24 20:23:06 +0100

    add use of NavLink and add some styles to recognize the current page

--- Frontend/src/pages/menu/index.tsx
+++ Frontend/src/pages/menu/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import ListItem from "@material-ui/core/ListItem";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 
@@ -15,29 +15,28 @@ export interface ItemProps {
 }
 
 const Menu: React.FC<Props> = ({ items }) => {
-  const history = useHistory();
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
-  const handleListItemClick = (index: number, url: string) => {
-    setSelectedIndex(index);
-    history.push(url);
-  };
-
   return (
     <>
       {items.map((mi, i) => (
-        <ListItem
-          key={i}
-          button
-          selected={selectedIndex === i}
-          onClick={() => handleListItemClick(i, mi.url)}
-        >
+        <StyledLink key={i} to={mi.url} activeClassName="active">
           <ListItemIcon>{mi.icon}</ListItemIcon>
           <ListItemText primary={mi.label} />
-        </ListItem>
+        </StyledLink>
       ))}
     </>
   );
 };
+
+const StyledLink = styled(NavLink)`
+  display: flex;
+  padding: 1rem;
+  align-items: center;
+  text-decoration: none;
+
+  &.active {
+    color: white;
+    background: rgb(123 110 171);
+  }
+`;
 
 export default Menu;
